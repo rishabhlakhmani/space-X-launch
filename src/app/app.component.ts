3e2834658148b8efadab1260ba875d01a445a31c
rishabh-lakhmani-windmill <rishabh.lakhmani@windmill.ch>
2020-10-18 21:34:15 +0530

    Metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    constructor(private titleService: Title, private metaTagService: Meta) { }
+
+    ngOnInit(): void {
+        this.titleService.setTitle('SpaceX Launch with Angular');
+        this.metaTagService.addTags([
+            { name: 'keywords', content: 'Angular SEO Integration, SpaceX Launch, Spacex APIs, Angular Universal, Angular 10' },
+            { name: 'robots', content: 'index, follow' },
+            { name: 'author', content: 'Rishabh Lakhmani' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { charset: 'UTF-8' }
+          ]);
+    }
 }
